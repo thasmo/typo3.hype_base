cf8158cd452da17ac9d3dacabbb40136d34c0245
Thomas Deinhamer <thasmo@gmail.com>
2011-01-28 22:28:40 +0000

    Fixes #12625.

--- Classes/Utility/TypoScriptGenerator.php
+++ Classes/Utility/TypoScriptGenerator.php
@@ -10,67 +10,67 @@ class Tx_HypeBase_Utility_TypoScriptGeneratorUtility {
 		'editorcfg.txt',
 		'include_static_file.txt'
 	);
-	
+
 	static public function createTypoScript() {
-		
+
 		# load TCA for the sys_template table
 		global $TCA;
 		t3lib_div::loadTCA('sys_template');
-		
+
 		# apply cache if set
 		if(count(self::$cache) > 0) {
 			$TCA['sys_template']['columns']['include_static_file']['config']['items'] = array_merge($TCA['sys_template']['columns']['include_static_file']['config']['items'], self::$cache);
 			return TRUE;
 		}
-		
+
 		# get extension configuration
 		$configuration = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['hype_base']);
-		
+
 		# clear local cache
 		self::$cache = array();
 		self::$layouts = array();
-		
+
 		# get configured path
 		$typoscriptPath = realpath(t3lib_div::getFileAbsFileName($configuration['typoscriptPath']));
-		
+
 		# get directory items
 		$items = new RecursiveIteratorIterator(new RecursiveDirectoryIterator($typoscriptPath), RecursiveIteratorIterator::LEAVES_ONLY);
-		
+
 		# loop thru items
 		foreach($items as $item) {
-			
+
 			# scan directory if files exists
 			if($item->isFile() && in_array($item->getFilename(), self::$files) && !in_array($item->getPath(), self::$cache)) {
-				
+
 				# add to cache
 				array_push(self::$cache, $item->getPath());
 			}
 		}
-		
+
 		# sort pathes
 		sort(self::$cache);
-		
+
 		# add pathes
 		foreach(self::$cache as $path) {
-			
+
 			# get relative directory path
-			$dir = str_replace($typoscriptPath . '/', '', $path);
-			
+			$dir = str_replace($typoscriptPath . DIRECTORY_SEPARATOR, '', $path);
+
 			# get current directory
-			$current = array_pop(explode('/', $dir));
-			
+			$current = array_pop(explode(DIRECTORY_SEPARATOR, $dir));
+
 			# skip if current directory name starts with an underscore
 			if(preg_match('~^_~', $current)) {
 				continue;
 			}
-			
+
 			$item = array(
-				trim('» ' . str_replace('/', ' › ', $dir)) . ' (hype_base)',
+				trim('» ' . str_replace(DIRECTORY_SEPARATOR, ' › ', $dir)) . ' (hype_base)',
 				$configuration['typoscriptPath'] . $dir
 			);
-			
+
 			$TCA['sys_template']['columns']['include_static_file']['config']['items'][] = $item;
-			
+
 			/*
 			# add to layout stack
 			if(preg_match('~^Layout\/~', $dir)) {
@@ -78,34 +78,34 @@ class Tx_HypeBase_Utility_TypoScriptGeneratorUtility {
 			}
 			*/
 		}
-		
+
 		/*
 		$data = 'TCEFORM.pages {
 			layout.removeItems = 1,2,3
-			
+
 			layout.altLabels.0 =';
-		
+
 		$constants = '';
 		$setup = '';
-		
+
 		foreach(self::$layouts as $key => $layout) {
 			$id = $key + 4;
-			
+
 			$data .= chr(10) . 'layout.addItems.' . $id . ' = ' . str_replace('/', ' › ', $layout);
-			
+
 			$constants .= '
 			[globalVar = TSFE:page|layout = ' . $id . ']
 			<INCLUDE_TYPOSCRIPT: source="FILE: typo3conf/ext/hype_base/Resources/Private/TypoScript/' . $layout . '/constants.txt">
 			[global]';
-			
+
 			$setup .= '
 			[globalVar = TSFE:page|layout = ' . $id . ']
 			<INCLUDE_TYPOSCRIPT: source="FILE: typo3conf/ext/hype_base/Resources/Private/TypoScript/' . $layout . '/setup.txt">
 			[global]';
 		}
-		
+
 		$data .= chr(10) . '}';
-		
+
 		t3lib_extMgm::addPageTSConfig($data);
 		t3lib_extMgm::addTypoScriptConstants($constants);
 		t3lib_extMgm::addTypoScriptSetup($setup);
