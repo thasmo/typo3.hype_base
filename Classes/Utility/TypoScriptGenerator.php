2c79d1ffb9f83b751c562960d3f9b115f43a8ac5
Thomas Deinhamer <thasmo@gmail.com>
2011-01-28 23:39:48 +0000

    Fixes #12627.

--- Classes/Utility/TypoScriptGenerator.php
+++ Classes/Utility/TypoScriptGenerator.php
@@ -112,4 +112,8 @@ class Tx_HypeBase_Utility_TypoScriptGeneratorUtility {
 		*/
 	}
 }
+
+if(defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/hype_base/Classes/Utility/TypoScriptGenerator.php']) {
+	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/hype_base/Classes/Utility/TypoScriptGenerator.php']);
+}
 ?>
